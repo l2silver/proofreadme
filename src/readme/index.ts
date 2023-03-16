2ba0f576d4a9c691bbad792b337eb254d63c83a9
Leigh Silverstein <l2silver@ryerson.ca>
2023-03-15 21:42:33 -0400

    fix(): Error message not showing when no readme file found
    The error is now issued when no readme file is found

--- src/readme/index.ts
+++ src/readme/index.ts
@@ -10,23 +10,19 @@ export default function getReadme(readmePath: string){
       throw Error(`Your readme file does not exist in the location you set: ${readmePath}`)
     }
   } else {
-    try {
-      if (fs.existsSync('README.md')) {
-        readme = fs.readFileSync('README.md', 'utf8');
-      }
-      if (fs.existsSync('readme.md')) {
-        readme = fs.readFileSync('readme.md', 'utf8');
-      }
-      if (fs.existsSync('Readme.md')) {
-        readme = fs.readFileSync('Readme.md', 'utf8');
-      }
-      if (fs.existsSync('ReadMe.md')) {
-        readme = fs.readFileSync('ReadMe.md', 'utf8');
-      }
-    } catch (err){
-      console.error('Your readme file does not exist where you ran proofreadme. Please specify its location using the --readme-path option.', readmePath)
-      throw err
+    if (fs.existsSync('README.md')) {
+      readme = fs.readFileSync('README.md', 'utf8');
     }
+    if (fs.existsSync('readme.md')) {
+      readme = fs.readFileSync('readme.md', 'utf8');
+    }
+    if (fs.existsSync('Readme.md')) {
+      readme = fs.readFileSync('Readme.md', 'utf8');
+    }
+    if (fs.existsSync('ReadMe.md')) {
+      readme = fs.readFileSync('ReadMe.md', 'utf8');
+    }
+    throw Error('Your readme file does not exist where you ran proofreadme. Please specify its location using the --readme-path option.')
   }
   return readme;
 }
